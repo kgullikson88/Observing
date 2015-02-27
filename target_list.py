447cc0ba0d8a15536d29926ee2cde69c71f3fce9
kgullikson88 <kevin.gullikson@gmail.com>
2015-02-27 10:44:32 -0600

    Added function to get follow-up targets from observed targets spreadsheet.

--- target_list.py
+++ target_list.py
@@ -11,20 +11,27 @@ def create_worklist(df):
      - spt (spectral type of the target. optional. string, if given)
      - epoch (optional. Assumes J2000 if not given)
     """
-    
+
     if 'spt' not in df.keys():
-    	df['spt'] = [''] * len(df)
+        df['spt'] = [''] * len(df)
     if 'epoch' not in df.keys():
-    	df['epoch'] = ['2000.0'] * len(df)
+        df['epoch'] = ['2000.0'] * len(df)
 
     for i, row in df.iterrows():
-    	if 'mag' in row.keys():
-    		outline = '{0:d}  "{1:.2f} {2:s} {3:s}  "   {4:s}  {5:s}  {6:s}  0.0  0.0'.format(i+1, row['mag'], row['spt'], row['Target'], 
-    			                                                                              row['RA'], row['DEC'], row['epoch'])
-    	else:
-    		outline = '{0:d}  "{2:s} {3:s}  "   {4:s}  {5:s}  {6:s}  0.0  0.0'.format(i+1, row['spt'], row['Target'], 
-    			                                                                      row['RA'], row['DEC'], row['epoch'])
-    	print(outline)
+        print i
+        print row['mag']
+        print row['epoch']
+        if 'mag' in row.keys():
+            outline = '{0:d}  "{1:.2f} {2:s} {3:s}  "   {4:s}  {5:s}  {6:s}  0.0  0.0'.format(i + 1, row['mag'],
+                                                                                              row['spt'], row['Target'],
+                                                                                              row['RA'], row['DEC'],
+                                                                                              row['epoch'])
+        else:
+            outline = '{0:d}  "{2:s} {3:s}  "   {4:s}  {5:s}  {6:s}  0.0  0.0'.format(i + 1, row['spt'], row['Target'],
+                                                                                      row['RA'], row['DEC'],
+                                                                                      row['epoch'])
+        print(outline)
+
 
 def Split_RA_DEC(string, divider=" "):
     """
@@ -32,14 +39,14 @@ def Split_RA_DEC(string, divider=" "):
     """
     if "+" in string:
         ra, dec = string.split("+")
-        lat="+"
+        lat = "+"
     elif "-" in string:
         ra, dec = string.split("-")
-        lat="-"
+        lat = "-"
     else:
-        #Something weird happened and there was no divider. Just return -1 for error purposes
+        # Something weird happened and there was no divider. Just return -1 for error purposes
         return -1, -1
-  
+
     ra_hours = ra.split()[0]
     ra_mins = ra.split()[1]
     ra_sec = ra.split()[2]
@@ -58,12 +65,11 @@ def get_unobserved():
     """
     Go through my sample spreadsheet, and find the targets which are not yet observed
     """
-    import HelperFunctions
     import os
 
     # Read in my target list
     filename = '{}/Dropbox/School/Research/AstarStuff/TargetLists/Final_Sample.xls'.format(os.environ['HOME'])
-    sample = pd.read_excel(filename, sheetname=3, header=8)[1:]
+    sample = pd.read_excel(filename, sheetname=3, header=8, na_values=['     ~'])[1:]
 
     # Keep only the un-observed targets
     good = sample.dropna(subset=['Observed?'])
@@ -76,13 +82,55 @@ def get_unobserved():
 
     # Rename some columns
     good.rename(columns={'Mag K': 'mag', 'identifier': 'Target', '  spec. type': 'spt'}, inplace=True)
-    
+
     return good
 
 
+def get_followup():
+    import os
+    import StarData
+
+    # Read in the observed target list
+    filename = '{}/Dropbox/School/Research/AstarStuff/TargetLists/Observed_Targets3.xls'.format(os.environ['HOME'])
+    sample = pd.read_excel(filename, sheetname=0, na_values=['     ~'])[1:]
+    sample['Mag K'] = sample.apply(lambda row: row['Mag K'] if pd.notnull(row['Mag K']) else row['Mag V'], axis=1)
+
+
+    # Get the targets with a detection
+    detections = sample.loc[sample['Detections'].notnull()]
+
+    # Get the detections without follow-up
+    followup = detections.groupby('identifier').filter(lambda df: df.shape[0] == 1)
+    followup.reset_index(inplace=True, drop=True)
+
+    # Split RA/DEC field
+    radec = followup['RA/DEC (J2000)'].map(Split_RA_DEC)
+    followup['RA'] = radec.map(lambda l: l[0])
+    followup['DEC'] = radec.map(lambda l: l[1])
+
+    # Rename columns
+    followup.rename(columns={'Mag K': 'mag', 'identifier': 'Target'}, inplace=True)
+
+    # Add the spectral type from simbad (not in the table for some silly reason)
+    followup['spectype'] = followup['Target'].map(lambda star: StarData.GetData(star).spectype)
+    followup['spt'] = followup.apply(lambda row: '{} + {}'.format(row['spectype'], row['Detections']), axis=1)
+
+    return followup
+
 
 if __name__ == '__main__':
-    df = get_unobserved()
-    
+    import sys
+
+    if '--new' in sys.argv[1:]:
+        df = get_unobserved()
+
+    elif '--follow-up' in sys.argv[1:]:
+        df = get_followup()
+
+    else:
+        print 'Must give one of the following command line arguments:'
+        print '  - "--new": for a worklist of un-observed targets'
+        print '  - "--follow-up": for a worklist of follow-up targets'
+
     # Print out the worklist
-    create_worklist(df[['Target', 'RA', 'DEC', 'mag']])
+    create_worklist(df[['Target', 'RA', 'DEC', 'mag', 'spt']])
